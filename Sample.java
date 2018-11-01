3f1153f8672b87704bf7ab1c32c2306cd506ed2a
BasmaBoulekhras <basmaboulekhras@gmail.com>
2018-11-01 14:09:12 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,34 @@
-   class Sample{ 
-   public static void main(String[] args)  { 
-   System.out.println("Hello World! ");
-   } 
-   }
+package demo;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+		
+		String string = "Hello world";
+		System.out.println(string);
+		
+		Logger log = Logger.getLogger(Application.class.getName());
+        log.log(Level.INFO, "Succes: "+string);
+        return string;
+    }
+
+    public static void main(String[] args) throws Exception {
+    	
+        SpringApplication.run(Application.class, args);
+    }
+}
